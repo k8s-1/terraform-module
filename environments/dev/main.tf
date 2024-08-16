05be9e5751f7d621e0a5b2b3c95aa6eed6462fc4
pman <pman-$-432894328923@gmail.com>
2024-08-16 23:56:38 +0200

    chore: update file(s): main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,4 +1,4 @@
-# name of module can be any name
+# name of resource can be anything
 module "custom_print_module" {
   source  = "../../modules/my-module"
   message = "Hello from Terraform Dev Environment!"

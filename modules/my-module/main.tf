556759d1128046b4110eabb292553b66c1e433a1
pman <pman-$-432894328923@gmail.com>
2024-08-16 23:35:18 +0200

    chore: update file(s): main.tf

---
+++ modules/my-module/main.tf
@@ -0,0 +1,11 @@
+resource "null_resource" "custom_print" {
+  provisioner "local-exec" {
+    command = "echo '${var.message}'"
+  }
+}
+
+resource "null_resource" "custom_print" {
+  provisioner "local-exec" {
+    command = "echo '${var.second_message}'"
+  }
+}

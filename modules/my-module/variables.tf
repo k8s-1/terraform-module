21498edeb50c773193416afe0a9deaff40d58d16
pman <pman-$-432894328923@gmail.com>
2024-08-16 23:36:02 +0200

    chore: update file(s): variables.tf

---
+++ modules/my-module/variables.tf
@@ -0,0 +1,11 @@
+variable "message" {
+  description = "The message to print"
+  type        = string
+  default     = "Hello, World!"
+}
+
+variable "second_message" {
+  description = "The message to print"
+  type        = string
+  default     = "A second message!"
+}
